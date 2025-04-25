c1d28f61a149cfafe68dfeef0a03c5ddb4b404bd
Alice <Grayhild@mail.ru>
2025-04-25 08:57:38 +0700

    commits

--- bot.py
+++ bot.py
@@ -12,30 +12,12 @@ cursor = conn.cursor()
 bot = telebot.TeleBot(config('TokenBot'))
 
 # Подключение к ИИ GigaChat от Сбера
-TOKEN = config('TokenChat')
-URL = 'https://api.gigachat.sber.ru/v1/chat/completions'
+from gigachat import GigaChat
 
-
-def send_request(prompt):
-    headers = {
-        'Authorization': f'Bearer {TOKEN}',
-        'Content-Type': 'application/json',
-    }
-    data = {
-        'model': 'gpt-3.5-turbo',  # Модель GigaChat, может зависеть от конфигурации
-        'messages': [
-            {'role': 'system', 'content': 'You are a helpful assistant.'},
-            {'role': 'user', 'content': prompt}
-        ]
-    }
-
-    response = requests.post(URL, json=data, headers=headers)
-
-    if response.status_code == 200:
-        return response.text
-    else:
-        print(f'Ошибка: {response.status_code}')
-        return None
+giga = GigaChat(
+   credentials=config('Authorization_key'),
+   verify_ssl_certs=False
+)
 
 
 # Работа с отправленными сообщениями пользователя в телеграм-боте
@@ -43,18 +25,21 @@ def send_request(prompt):
 def get_text_messages(message):
     if message.text == "/start":
         bot.send_message(message.from_user.id, "Привет! Бот работает!")
-    if message.text:
-
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Введи любое сообщение, и GigaChat тебе ответит!  ;)")
+    else:
         # Подключение GigaChat для обработки запроса пользователя в телеграм-боте
         if __name__ == '__main__':
+            giga.get_token()
             user_prompt = message.text
-            result = send_request(user_prompt)
-            if result:
-                answer = result['choices'][0]['message']['content']
-                bot.send_message(message.from_user.id, answer)
+            response = giga.chat(user_prompt)
+            response_chat = response.choices[0].message.content
+            if response_chat:
+                bot.send_message(message.from_user.id, response_chat)
                 # Занесение запроса в базу данных
-                data = (message.from_user.id, message.text, answer)
+                data = (message.from_user.id, message.text, response_chat)
                 cursor.execute("INSERT INTO history (chat_user, user_text, answer) VALUES (%s, %s, %s)", data)
+                conn.commit()
 
 # Проверка наличия сообщений телеграм-бота
 bot.polling(none_stop=True, interval=0)
